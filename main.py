d6a5cb2c6483a1e0b7598562540fe1ecab39c2ea
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-06-30 18:17:12 -0700

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # TODO 1: Create snake body
 # TODO 2: Move the snake
 # TODO 3: Control the snake
+# TODO 4:
 
 from turtle import Screen, Turtle
 import time
 from snake import Snake
+
+game_is_on = True  # Declare game_is_on as global
 
 screen = Screen()
 screen.setup(600, 600)
@@ -14,10 +17,19 @@ screen.tracer(0)
 
 snake = Snake()
 
-game_is_on = True
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
 while game_is_on:
+    # Update the screen every 0.1 seconds
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.5)
     snake.move()
 
 
